5e6f5db63c3cdc3a04736caec46e580df6a4a8bc
Prem Nirmal <prem@fuzzproductions.com>
2014-05-09 13:25:37 -0400

    catching and logging sqliteException for migrations

--- src/main/java/com/activeandroid/DatabaseHelper.java
+++ src/main/java/com/activeandroid/DatabaseHelper.java
@@ -16,6 +16,15 @@ package com.activeandroid;
  * limitations under the License.
  */
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.activeandroid.util.AALog;
+import com.activeandroid.util.NaturalOrderComparator;
+import com.activeandroid.util.SQLiteUtils;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -27,161 +36,154 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-
-import com.activeandroid.util.AALog;
-import com.activeandroid.util.NaturalOrderComparator;
-import com.activeandroid.util.SQLiteUtils;
-
 public final class DatabaseHelper extends SQLiteOpenHelper {
-	//////////////////////////////////////////////////////////////////////////////////////
-	// PUBLIC CONSTANTS
-	//////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////
+    // PUBLIC CONSTANTS
+    //////////////////////////////////////////////////////////////////////////////////////
 
-	public final static String MIGRATION_PATH = "migrations";
+    public final static String MIGRATION_PATH = "migrations";
 
-	//////////////////////////////////////////////////////////////////////////////////////
-	// CONSTRUCTORS
-	//////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////
+    // CONSTRUCTORS
+    //////////////////////////////////////////////////////////////////////////////////////
 
-	public DatabaseHelper(Configuration configuration) {
-		super(configuration.getContext(), configuration.getDatabaseName(), null, configuration.getDatabaseVersion());
-		copyAttachedDatabase(configuration.getContext(), configuration.getDatabaseName());
-	}
+    public DatabaseHelper(Configuration configuration) {
+        super(configuration.getContext(), configuration.getDatabaseName(), null, configuration.getDatabaseVersion());
+        copyAttachedDatabase(configuration.getContext(), configuration.getDatabaseName());
+    }
 
-	//////////////////////////////////////////////////////////////////////////////////////
-	// OVERRIDEN METHODS
-	//////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////
+    // OVERRIDEN METHODS
+    //////////////////////////////////////////////////////////////////////////////////////
 
-	@Override
-	public void onOpen(SQLiteDatabase db) {
-		executePragmas(db);
-	};
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        executePragmas(db);
+    }
 
-	@Override
-	public void onCreate(SQLiteDatabase db) {
-		executePragmas(db);
-		executeCreate(db);
-		executeMigrations(db, -1, db.getVersion());
-	}
+    ;
 
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		executePragmas(db);
-		executeCreate(db);
-		executeMigrations(db, oldVersion, newVersion);
-	}
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        executePragmas(db);
+        executeCreate(db);
+        executeMigrations(db, -1, db.getVersion());
+    }
 
-	//////////////////////////////////////////////////////////////////////////////////////
-	// PUBLIC METHODS
-	//////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        executePragmas(db);
+        executeCreate(db);
+        executeMigrations(db, oldVersion, newVersion);
+    }
 
-	public void copyAttachedDatabase(Context context, String databaseName) {
-		final File dbPath = context.getDatabasePath(databaseName);
+    //////////////////////////////////////////////////////////////////////////////////////
+    // PUBLIC METHODS
+    //////////////////////////////////////////////////////////////////////////////////////
 
-		// If the database already exists, return
-		if (dbPath.exists()) {
-			return;
-		}
+    public void copyAttachedDatabase(Context context, String databaseName) {
+        final File dbPath = context.getDatabasePath(databaseName);
 
-		// Make sure we have a path to the file
-		dbPath.getParentFile().mkdirs();
+        // If the database already exists, return
+        if (dbPath.exists()) {
+            return;
+        }
 
-		// Try to copy database file
-		try {
-			final InputStream inputStream = context.getAssets().open(databaseName);
-			final OutputStream output = new FileOutputStream(dbPath);
+        // Make sure we have a path to the file
+        dbPath.getParentFile().mkdirs();
 
-			byte[] buffer = new byte[1024];
-			int length;
+        // Try to copy database file
+        try {
+            final InputStream inputStream = context.getAssets().open(databaseName);
+            final OutputStream output = new FileOutputStream(dbPath);
 
-			while ((length = inputStream.read(buffer)) > 0) {
-				output.write(buffer, 0, length);
-			}
+            byte[] buffer = new byte[1024];
+            int length;
 
-			output.flush();
-			output.close();
-			inputStream.close();
-		}
-		catch (IOException e) {
-			AALog.e("Failed to open file", e);
-		}
-	}
+            while ((length = inputStream.read(buffer)) > 0) {
+                output.write(buffer, 0, length);
+            }
 
-	//////////////////////////////////////////////////////////////////////////////////////
-	// PRIVATE METHODS
-	//////////////////////////////////////////////////////////////////////////////////////
+            output.flush();
+            output.close();
+            inputStream.close();
+        } catch (IOException e) {
+            AALog.e("Failed to open file", e);
+        }
+    }
 
-	private void executePragmas(SQLiteDatabase db) {
-		if (SQLiteUtils.FOREIGN_KEYS_SUPPORTED) {
-			db.execSQL("PRAGMA foreign_keys=ON;");
-			AALog.i("Foreign Keys supported. Enabling foreign key features.");
-		}
-	}
+    //////////////////////////////////////////////////////////////////////////////////////
+    // PRIVATE METHODS
+    //////////////////////////////////////////////////////////////////////////////////////
 
-	private void executeCreate(SQLiteDatabase db) {
-		db.beginTransaction();
-		try {
-			for (TableInfo tableInfo : Cache.getTableInfos()) {
-				db.execSQL(SQLiteUtils.createTableDefinition(tableInfo));
-			}
-			db.setTransactionSuccessful();
-		}
-		finally {
-			db.endTransaction();
-		}
-	}
+    private void executePragmas(SQLiteDatabase db) {
+        if (SQLiteUtils.FOREIGN_KEYS_SUPPORTED) {
+            db.execSQL("PRAGMA foreign_keys=ON;");
+            AALog.i("Foreign Keys supported. Enabling foreign key features.");
+        }
+    }
 
-	private boolean executeMigrations(SQLiteDatabase db, int oldVersion, int newVersion) {
-		boolean migrationExecuted = false;
-		try {
-			final List<String> files = Arrays.asList(Cache.getContext().getAssets().list(MIGRATION_PATH));
-			Collections.sort(files, new NaturalOrderComparator());
+    private void executeCreate(SQLiteDatabase db) {
+        db.beginTransaction();
+        try {
+            for (TableInfo tableInfo : Cache.getTableInfos()) {
+                db.execSQL(SQLiteUtils.createTableDefinition(tableInfo));
+            }
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+        }
+    }
 
-			db.beginTransaction();
-			try {
-				for (String file : files) {
-					try {
-						final int version = Integer.valueOf(file.replace(".sql", ""));
+    private boolean executeMigrations(SQLiteDatabase db, int oldVersion, int newVersion) {
+        boolean migrationExecuted = false;
+        try {
+            final List<String> files = Arrays.asList(Cache.getContext().getAssets().list(MIGRATION_PATH));
+            Collections.sort(files, new NaturalOrderComparator());
 
-						if (version > oldVersion && version <= newVersion) {
-							executeSqlScript(db, file);
-							migrationExecuted = true;
+            db.beginTransaction();
+            try {
+                for (String file : files) {
+                    try {
+                        final int version = Integer.valueOf(file.replace(".sql", ""));
 
-							AALog.i(file + " executed succesfully.");
-						}
-					}
-					catch (NumberFormatException e) {
-						AALog.w("Skipping invalidly named file: " + file, e);
-					}
-				}
-				db.setTransactionSuccessful();
-			}
-			finally {
-				db.endTransaction();
-			}
-		}
-		catch (IOException e) {
-			AALog.e("Failed to execute migrations.", e);
-		}
+                        if (version > oldVersion && version <= newVersion) {
+                            executeSqlScript(db, file);
+                            migrationExecuted = true;
 
-		return migrationExecuted;
-	}
+                            AALog.i(file + " executed succesfully.");
+                        }
+                    } catch (NumberFormatException e) {
+                        AALog.w("Skipping invalidly named file: " + file, e);
+                    }
+                }
+                db.setTransactionSuccessful();
+            } finally {
+                db.endTransaction();
+            }
+        } catch (IOException e) {
+            AALog.e("Failed to execute migrations.", e);
+        }
 
-	private void executeSqlScript(SQLiteDatabase db, String file) {
-		try {
-			final InputStream input = Cache.getContext().getAssets().open(MIGRATION_PATH + "/" + file);
-			final BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-			String line = null;
+        return migrationExecuted;
+    }
 
-			while ((line = reader.readLine()) != null) {
-				db.execSQL(line.replace(";", ""));
-			}
-		}
-		catch (IOException e) {
-			AALog.e("Failed to execute " + file, e);
-		}
-	}
+    private void executeSqlScript(SQLiteDatabase db, String file) {
+        try {
+            final InputStream input = Cache.getContext().getAssets().open(MIGRATION_PATH + "/" + file);
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            String line = null;
+
+            while ((line = reader.readLine()) != null) {
+                try {
+                    db.execSQL(line.replace(";", ""));
+                } catch (SQLiteException ignored) {
+                    AALog.e("Failed to execute " + file, ignored);
+                }
+
+            }
+        } catch (IOException e) {
+            AALog.e("Failed to execute " + file, e);
+        }
+    }
 }
